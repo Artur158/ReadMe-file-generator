80439add9d8a72398a56fd3469b54b3d470347fd
Artur <artur.piasecki158@gmail.comgit config --global user.email artur.piasecki158@gmail.comgit config --list$ git config --global user.email artur.piasecki158@gmail.com>
2024-02-20 23:08:13 +0000

    new

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const generateLicenseBadge = license => {
   return license !== 'None' ? `![License](https://img.shields.io/badge/license-${license}-blue.svg)` : '';
 };
@@ -36,6 +37,41 @@ ${data.contributing}
 ${data.tests}
 
 ## Questions
+=======
+const generateMarkdown = data => {
+  return `
+# ${data.title}
+
+
+# Description
+${data.description}
+
+# Table of Contents
+1. Description
+2. Installation
+3. Usage
+4. License
+5. Contributing
+6.Tests
+7. Questions 
+
+# Install
+${data.installation}
+
+# Usage
+${data.usage}
+
+# License
+This project is licensed under the ${data.license} license.
+
+# Contributing
+${data.contributing}
+
+#Tests
+${data.tests}
+
+# Questions
+>>>>>>> f4e6990fce4961d36e1280e5c72bd0ed1c9edcca
 For questions about the project, feel free to contact me via my [GitHub profile](https://github.com/${data.github}) or via email at ${data.email}.
 `;
 };
