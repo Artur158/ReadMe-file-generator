a6627fc3026668cc69be04d2bf521a04bd48a5c3
Artur <artur.piasecki158@gmail.comgit config --global user.email artur.piasecki158@gmail.comgit config --list$ git config --global user.email artur.piasecki158@gmail.com>
2024-02-20 22:10:42 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
+
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
+
+
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const readmeContent = generateMarkdown(answers);
+            writeToFile('README.md', readmeContent);
+            console.log('README.md generated successfully!');
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
+}
+
+
+init();
